5896e0e969e7ab0d059f9392cb730dc35e63ccbe
Ruchita Suranagi <55045069+RuchitaSuranagi@users.noreply.github.com>
2021-10-01 18:23:14 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// responsive menu
+let menu = document.querySelector('#menu_logo');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
